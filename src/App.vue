cd777ccb968457e57e046a0a0a80de039ff464db
kurokawakazuhiro <k.kurokawa11@gmail.com>
2021-07-21 23:06:37 +0900

    チェックボックスの挙動の確認

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,13 @@
       <label for="detail">イベントの内容</label>
       <textarea id="detail" cols="30" rows="10" v-model="detail"></textarea>
       <p>{{ eventData.detail }}</p>
+      <input 
+      type="checkbox"
+      id="isPrivate"
+      v-model="eventData.isPrivate"
+      >
+      <label for="isPrivate">非公開</label>
+      <p>{{eventData.isPrivate}}</p>
     </div>
   </div>
 </template>
@@ -36,7 +43,8 @@ export default {
         title: "タイトル",
         maxNumber: 0,
         host: "",
-        detail: ""
+        detail: "",
+        isPrivate: false
       }
     }
   },
